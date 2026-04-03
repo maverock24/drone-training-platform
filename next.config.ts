a0b7327336524b4f02111400aa6f97d8204057c1
Maverock24 <test@test.fi>
2026-04-03 21:55:41 +0300

    fix(config): remove deprecated eslint settings from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors. We ignore for edge rapid deployment.
-    ignoreDuringBuilds: false,
-  },
-  typescript: {
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: false,
-  },
 };
 
 export default nextConfig;
